322ae390fc3fbabca07c6c1efe163d3b21a1c7ee
MarwanDev <marwaneg@aucegypt.edu>
2023-04-19 08:54:49 +0200

    Add popup close button functionality

--- src/modules/renderPopup.js
+++ src/modules/renderPopup.js
@@ -5,7 +5,11 @@ const renderPopup = (array) => {
     const catCard = document.getElementById(`card-${element._id}`);
     catCard.addEventListener('click', () => {
       const popUp = document.getElementById(`popup-${element._id}`);
+      const closePopup = document.getElementById(`close-popup-${element._id}`);
       popUp.style.display = 'flex';
+      closePopup.addEventListener('click', () => {
+        popUp.style.display = 'none';
+      });
     });
   });
 };
