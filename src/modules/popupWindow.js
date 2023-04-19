fcf626b0bffd3c7b8413d38eed416817dd8525bf
yacoubou-seidou <yacoseidou@gmail.com>
2023-04-19 14:57:28 +0100

    update view for modal

--- src/modules/popupWindow.js
+++ src/modules/popupWindow.js
@@ -10,16 +10,13 @@ const itemPopup = (_id, tags, owner, createdAt, updatedAt, index) => `
     </span>
   <div class="popup-cat-image"></div>
   <p class="popup-title">Cat ${index}</p>
-  <div class="tags-container" id="tagContainer">
-    <div class="tag">${tags[0]}</div>
-    <div class="tag">${tags[1]}</div>
-    <div class="tag">${tags[3]}</div>
-    <div class="tag">${tags[4]}</div>
-  </div>
+  <ul class="tags-container" id="tagContainer">
+  ${tags}
+  </ul>
   <div class="popup-desc-container">
     <h2>Owner: ${owner}</h2>
-    <h2>Created At: ${createdAt}</h2>
-    <h2>Updated At: ${updatedAt}</h2>
+    <h2>Created At: ${new Date(createdAt).toLocaleString()}</h2>
+    <h2>Updated At: ${new Date(updatedAt).toLocaleString()}</h2>
   </div>
   <div class="comments-section">
     <h3>Comments</h3>
@@ -41,11 +38,21 @@ const itemPopup = (_id, tags, owner, createdAt, updatedAt, index) => `
 `;
 const displayPopup = (array) => {
   array.forEach((element, index) => {
+    let { owner } = element;
+    let tags = '';
+    if (owner === 'null') {
+      owner = 'No owner';
+    }
+    element.tags.forEach((el, index) => {
+      if (index < 4) {
+        tags += `<li class='tag'> ${el}</li>`;
+      }
+    });
     const popupHtml = itemPopup(
       // eslint-disable-next-line no-underscore-dangle
       element._id,
-      element.tags,
-      element.owner,
+      tags,
+      owner,
       element.createdAt,
       element.updatedAt,
       index,
